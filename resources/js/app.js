1511935281379291233336edd2309eeb4e722b37
Max <kuzminitb@gmail.com>
2024-04-19 10:54:23 +0600

    init

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import ExampleComponent from "./components/ExampleComponent.vue";
+
+require('./bootstrap');
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        ExampleComponent: ExampleComponent
+    }
+})
