3c3e0abb405fe41e889e7e540130aa6190b2a9d8
Max <kuzminitb@gmail.com>
2024-04-19 18:00:26 +0600

    12 lesson

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -5,6 +5,26 @@ export default {
     name: "PostComponent",
     data() {
         return {
+            persons: [
+                {
+                    id: 1,
+                    name: 'Ivan',
+                    age: 25,
+                    job: 'engineer'
+                },
+                {
+                    id: 2,
+                    name: 'Sergey',
+                    age: 42,
+                    job: 'coach'
+                },
+                {
+                    id: 3,
+                    name: 'Liza',
+                    age: 17,
+                    job: 'designer'
+                }
+            ]
         }
     },
     methods: {
@@ -19,6 +39,24 @@ export default {
 
 <template>
     <div>
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Job</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in persons">
+                <th scope="row">{{ person.id }}</th>
+                <td>{{ person.name }}</td>
+                <td>{{ person.age }}</td>
+                <td>{{ person.job }}</td>
+            </tr>
+            </tbody>
+        </table>
         <SinglePostComponent/>
     </div>
 </template>
